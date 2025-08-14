f50b72435944d1292925a5e113a58090fcca98d2
ahmadashayer44 <ashayera44@gmail.com>
2025-08-14 15:47:57 +0300

    Rendring users using map method

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import User from "./components/User";
 import Buttons from "./components/Buttons";
 import "./App.css";
 function App() {
+  let users = [
+    { name: "Caroline Bloeme", checked: false },
+    { name: "Sun Jun", checked: true },
+    { name: "Song Bao", checked: false },
+    { name: "Olivia Arribas", checked: true },
+    { name: "Bonginkosi Mdladlana", checked: false },
+    { name: "Arina Belomestnykh", checked: true },
+    { name: "Jacqueline Likoki", checked: true },
+  ];
   return (
     <div className="App-header">
       <h1 className="App-title">Add members to Front-End development team</h1>
       <div className="App-body">
         <SearchBar />
-        <User name="Caroline Bloeme" checked={false} />
-        <User name="Sun Jun" checked={true} />
-        <User name="Song Bao" checked={false} />
-        <User name="Olivia Arribas" checked={true} />
-        <User name="Bonginkosi Mdladlana" checked={false} />
-        <User name="Arina Belomestnykh" checked={true} />
-        <User name="Jacqueline Likoki" checked={true} />
+        {users.map((user) => {
+          return <User name={user.name} checked={user.checked} />;
+        })}
         <Buttons />
       </div>
     </div>
